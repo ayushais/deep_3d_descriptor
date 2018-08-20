995715fe4fa337f0da1eeb305693df10c246ca9c
Ayush Dewan <dewan@informatik.uni-freiburg.de>
2018-08-20 10:57:23 +0200

    changed the download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -3,7 +3,11 @@ mkdir models
 cd models
 wget deep3d-descriptor.informatik.uni-freiburg.de/models/deep_3d_descriptor_matching.zip
 unzip deep_3d_descriptor_matching.zip
+mv deep_3d_descriptor_matching/* .
+rm -r deep_3d_descriptor_matching/
 rm deep_3d_descriptor_matching.zip
 wget deep3d-descriptor.informatik.uni-freiburg.de/models/deep_3d_descriptor_hinge_loss.zip
 unzip deep_3d_descriptor_hinge_loss.zip
+mv deep_3d_descriptor_hinge_loss/* .
+rm -r deep_3d_descriptor_hinge_loss/
 rm deep_3d_descriptor_hinge_loss.zip
